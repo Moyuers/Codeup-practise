354c9a049df2dd632dca96cd0b389670afe793c8
Moyuers <31651391+Moyuers@users.noreply.github.com>
2018-08-16 22:40:50 +0800

    Create 1135-c-1.1.cpp

---
+++ 1135-c-1.1.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+//1135.C语言1.1
+/*题目描述
+输出一行信息。
+
+输入
+ 无。
+
+输出
+只有一行，“This is a C program.”。
+
+请注意不需要输出引号，行尾输出换行。
+
+样例输入
+无。
+样例输出
+This is a C program.*/
+
+int main(){
+    printf("This is a C program.\n");
+    return 0;
+}
