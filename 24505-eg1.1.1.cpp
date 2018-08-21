491d65f68d1084fbcd650c5964dee769c5f4fde7
Moyuers <31651391+Moyuers@users.noreply.github.com>
2018-08-21 23:17:50 +0800

    Create 24505-eg1.1.1.cpp

---
+++ 24505-eg1.1.1.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+//24505.例题1-1-1 按要求输出信息(1)
+/*题目描述
+编写一个C程序，要求在屏幕上输出一下一行信息。
+
+This is my first c program!
+
+输入
+无
+输出
+This is my first c program!
+样例输入
+无
+样例输出
+This is my first c program!*/
+
+int main(){
+    printf("This is my first c program!\n");
+   return 0;
+}
